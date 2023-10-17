b03c80a6b0e320f10e25cb18760aa25bc703038c
Tibira Botto <tibira.botto@gmail.com>
2023-10-16 22:31:14 -0400

    assert

---
+++ assertEqual.js
@@ -0,0 +1,8 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    return actual === expected
+};
+
+// TEST CODE
+console.assert(assertEqual("Lighthouse Labs", "Bootcamp"))
+console.assert(assertEqual(1, 1))
