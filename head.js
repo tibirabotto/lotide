811292ec1324e07fb11325d94c47143367a78c74
Tibira Botto <tibira.botto@gmail.com>
2023-10-16 23:19:17 -0400

    assetE

--- head.js
+++ head.js
@@ -1,7 +1,10 @@
+var assertEqual = require('assert');
+
 // FUNCTION IMPLEMENTATION
-const head = function(actual, expected) {
-    if (actual.length > 0) {
-        return actual.indexOf(expected)
+const head = function(actual) {
+
+    if (actual.length > 0) {       
+        return actual[0]
     } else {
         return undefined
     }
@@ -9,7 +12,7 @@ const head = function(actual, expected) {
 };
 
 // TEST CODE
-console.assert(head([5,6,7], 5) === 0)
-console.assert(head(["Hello", "Lighthouse", "Labs"],  "Hello") === 0)
-console.assert(head([],  "Hello") === undefined)
+assertEqual.equal(head([5,6,7]), 5); 
+assertEqual.equal(head(["Hello", "Lighthouse", "Labs"]),  "Hello"); 
+assertEqual.equal(head([]),  undefined)
 
