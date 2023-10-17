644b758bfd2b3e69da84b9ccfa20fb30e6fbc617
Tibira Botto <tibira.botto@gmail.com>
2023-10-16 23:27:55 -0400

    tail

---
+++ tail.js
@@ -0,0 +1,11 @@
+var assertEqual = require('assert');
+var _ = require("lodash");
+
+// FUNCTION IMPLEMENTATION
+const tail = function(actual) {
+
+    return _.tail(actual);
+    
+};
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual.equal(result.length, 2); 
