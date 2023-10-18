091499a06c2b72fc86e96c637a923a6e3a4bf1f0
Tibira Botto <tibira.botto@gmail.com>
2023-10-17 21:54:33 -0400

    eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+var assertEqual = require('assert');
+var _ = require("lodash");
+
+
+const eqArrays = (arr1, arr2) => {
+    return _.isEqual(arr1, arr2);
+}
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]))// => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual.equal(eqArrays([1, 2, 3], [1, 2, 3]), true)
+
+
