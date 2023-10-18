bc273b179f6c31e62351fe1e793e161b4618a5f3
Tibira Botto <tibira.botto@gmail.com>
2023-10-17 22:24:49 -0400

    rectatory

--- without.js
+++ without.js
@@ -4,7 +4,13 @@ var _ = require("lodash");
 
 const without = (arr1, arr2) => {
     const val = arr2[0]
-    return _.without(arr1, val)
+    let result = []
+    for (let arr of arr1) {
+        if (arr !== val) {
+            result.push(arr)
+        }
+    }
+    return result
 }
 
 
@@ -12,3 +18,4 @@ const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+assertArraysEqual(_.isEqual(without(words, ["lighthouse"]), ["hello", "world"]))
